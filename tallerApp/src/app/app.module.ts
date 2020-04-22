c17d70ce0a8f215ffd8ee33786d3c59a72440046
dennis.castro <manuel.castro.guzman@ericsson.com>
2020-04-21 23:03:19 -0600

    rutas

--- tallerApp/src/app/app.module.ts
+++ tallerApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
